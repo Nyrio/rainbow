97744727a7353f0a2f372d75c5de65b99cd3d522
Louis Sugy <17441062+Nyrio@users.noreply.github.com>
2015-05-21 15:32:26 +0200

    Fusion des travaux de Peio et moi.

--- util.ml
+++ util.ml
@@ -1,3 +1,34 @@
+open Cryptokit
+
+let sha256 = hash_string (Hash.sha256 ())
+let sha512 = hash_string (Hash.sha512 ())
+
+let mk_rdx charset length =
+    let n_char = String.length charset in
+    fun salt h ->
+        let res = Bytes.create length in
+        for i = 0 to length - 1 do
+            Bytes.set res i charset.[(int_of_char h.[i] lxor salt) mod n_char];
+        done;
+        res
+
+
+module RC4SeedEnum = struct
+    let type t = string * int
+    let type elt = string
+    let type enum = transform * int
+
+    let start (seed, n) = (Cipher.arcfour seed Cipher.encrypt, n)
+
+    let null = String.make 32 '\000'
+    let step (arcfour, n) = match n with
+    | 0 -> None
+    | _ ->
+       let () = arcfour#put_string null in
+       Some (arcfour#get_string, arcfour)
+end
+
+
 (* Fonction dichotomique qui prend en argument un tableau t de bornes d'intervalles d'éléments de type 'a classés par ordre lexicographique croissant et associe à l'élément s de type 'a le numéro de l'intervalle auquel il appartient, entre b1 et b2. *)
 let rec dicho_bornes (t : 'a vect) (s : 'a) (b1 : int) (b2 : int) =
   let m = (b1 + b2) / 2
