de0d4709ba2b12d5d91256eb092fbd9c0e078611
Louis Sugy <17441062+Nyrio@users.noreply.github.com>
2015-05-28 15:29:18 +0200

    Fonctions d'enregistrement des morceaux de la table dans des fichiers.

--- util.ml
+++ util.ml
@@ -36,16 +36,3 @@ module RC4SeedEnum(S:Size) = struct
        let () = arcfour#put_string null in
        Some (arcfour#get_string, arcfour)
 end
-
-
-(* Fonction dichotomique qui prend en argument un tableau t de bornes d'intervalles d'éléments de type 'a classés par ordre lexicographique croissant et associe à l'élément s de type 'a le numéro de l'intervalle auquel il appartient, entre b1 et b2. *)
-let rec dicho_bornes (t : 'a array) (s : 'a) (b1 : int) (b2 : int) =
-  let m = (b1 + b2) / 2
-  in if b1 == m
-     then b1
-     else if s < t.(m)
-          then dicho_bornes t s b1 m
-          else dicho_bornes t s m b2;;
-
-(* test : dicho_bornes [| 0; 10; 20; 30; 40; 50; 60; 70; 80; 90; 100 |] 100 0 10;; *)
-(* test : dicho_bornes [| "aaaa"; "dddd"; "llll"; "oooo"; "ssss"; "zzzz" |] "ozaml" 0 5;; *)
