38257c2beef3cf0e31be54b52a33ea9013577678
Lapinot <Lapin0t@users.noreply.github.com>
2016-01-19 16:25:52 +0100

    Updated test scripts to the new table interface.

--- utils/test_tbl.ml
+++ utils/test_tbl.ml
@@ -1,15 +1,20 @@
-let rec pws_of_seeds rdx_f step state = match step state with
-| None -> []
-| Some (seed, state) -> rdx_f 0 seed :: pws_of_seeds rdx_f step state
-
-let test table pw = match Table.crack table (table.hash_f pw) with
-| None -> print_endline (pw ^ ": not found")
-| Some x -> assert (x = pw); print_endline (pw ^ ": found")
-
 let () =
     let in_file = Sys.argv.(1) in
-    let table = Table.load in_file in
+    let header = SimpleTable.read_header in_file in
 
-    let seed_enum = Crypto.rc4seeds (Crypto.get_hash_len table.hash_id) (int_of_string Sys.argv.(2)) in
-    let pws = pws_of_seeds table.rdx_f seed_enum.step (seed_enum.start ()) in
-    List.iter (test table) pws;
+    let module F = (val SimpleTable.make_fct header) in
+    let module T = Table.Make(F) in
+
+    let table = SimpleTable.load in_file in
+    let seed_enum = Crypto.random_seeds (Crypto.get_hash_len header.hash_id) (int_of_string Sys.argv.(2)) in
+
+    let rec pws_of_seeds rdx_f step state = match step state with
+    | None -> []
+    | Some (seed, state) -> rdx_f 0 seed :: pws_of_seeds rdx_f step state in
+
+    let test table pw = match T.crack table (F.hash_f pw) with
+    | None -> print_endline (pw ^ ": not found")
+    | Some x -> assert (x = pw); print_endline (pw ^ ": found") in
+
+    let pws = pws_of_seeds (F.rdx_f) seed_enum.step (seed_enum.start ()) in
+    List.iter (test (SimpleTable.lookup table)) pws
