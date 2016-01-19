01abbc7c97e0bdeca77efccd9c52ce9aba0d7836
ho33e5 <ho33e5@gmail.com>
2016-01-19 16:25:52 +0100

    Updated test scripts to the new table interface.

--- utils/gen_mono.ml
+++ utils/gen_mono.ml
@@ -1,35 +1,39 @@
+open SimpleTable
+
 let rec pws_of_seeds rdx_f step state = match step state with
 | None -> []
 | Some (seed, state) -> rdx_f 0 seed :: pws_of_seeds rdx_f step state
 
-let test table pw = match Table.crack table (table.hash_f pw) with
-| None -> print_endline (pw ^ ": not found")
-| Some _ -> print_endline (pw ^ ": found")
-
-
-let rec populate tbl step state = match step state with
-| None -> ()
-| Some (seed, state) -> Table.add_chain tbl seed; populate tbl step state
-
 
 let () =
     let argv = Sys.argv in
-    let chain_len = int_of_string argv.(1) in
+    let header = {chain_len = int_of_string argv.(1);
+                  hash_id = argv.(3); charset = argv.(4);
+                  pw_len = int_of_string argv.(5)} in
     let chain_num = int_of_string argv.(2) in
-    let hash_id = argv.(3) in
-    let rdx_id = (argv.(4), int_of_string argv.(5)) in
     let out_file = argv.(6) in
 
-    let table = Table.create chain_len hash_id rdx_id (2*chain_num) in
-    let seed_enum = Crypto.rc4seeds (Crypto.get_hash_len hash_id) chain_num in
+    let module F = (val SimpleTable.make_fct header) in
+    let module T = Table.Make(F) in
+
+    let table = SimpleTable.create header (2*chain_num) in
+    let seed_enum = Crypto.random_seeds (Crypto.get_hash_len header.hash_id) chain_num in
+
+    let test table pw = match T.crack table (F.hash_f pw) with
+    | None -> print_endline (pw ^ ": not found")
+    | Some _ -> print_endline (pw ^ ": found") in
+
+
+    let rec populate tbl step state = match step state with
+    | None -> ()
+    | Some (seed, state) -> SimpleTable.add tbl (T.endpoint seed 0) seed; populate tbl step state in
 
     populate table seed_enum.step (seed_enum.start ());
 
-    let seed_enum = Crypto.rc4seeds (Crypto.get_hash_len table.hash_id) 100 in
-    let pws = pws_of_seeds table.rdx_f seed_enum.step (seed_enum.start ()) in
-    List.iter (test table) pws;
+    let seed_enum = Crypto.random_seeds (Crypto.get_hash_len table.header.hash_id) 100 in
+    let pws = pws_of_seeds F.rdx_f seed_enum.step (seed_enum.start ()) in
+    List.iter (test (SimpleTable.lookup table)) pws;
 
 
 
-    Table.dump table out_file;
-
+    SimpleTable.dump table out_file;
